d62fda80ab60e5546a720b1d5ba1d7a167f98001
Alberto Torres <alberto.torres@uam.es>
2018-04-24 11:09:16 +0200

    First commit

---
+++ aps.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+import numpy as np
+from scipy.stats import bernoulli
+from scipy.stats import mode
+
+
+########### APS auxiliar function #################################
+def innerAPS(d_given, a_values, a_util, theta, N_inner=1000, burnin=0.75):
+    """ Computes the attacker's solution in an attacker-defender game using APS
+
+        Parameters
+        ----------
+        d_given : Integer or string
+            Decision of the defender
+        a_values : array-like
+            Strategies for the attacker
+        a_util : callable
+            Computes the utility of the attacker
+
+                ``a_util(a, theta) -> array, shape (same as theta)``
+
+            where theta is a 1-D array and a is the decision of the attacker
+        theta : callable
+            Draws samples of theta from the distribution p(theta | d, a)
+
+                ``theta(d, a, size=1) -> array, shape (size,)``
+
+            where d and a are the decisions for the defender and the attacker
+        N_inner : int, optional (default=1000)
+            Number of iterations for the APS algorithm
+        burnin : float, optional (default=0.75)
+            Percentage of burn-in iterations in APS inner algorithms
+    """
+    a_sim = np.zeros(N_inner, dtype = int)
+    a_sim[0] = np.random.choice(a_values)
+    theta_sim = theta(d_given, a_sim[0])
+
+    def propose(a_given, a_values):
+        return np.random.choice(a_values[~(a_values == a_given)])
+
+    for i in range(1,N_inner):
+        ## Update a
+        a_tilde = propose(a_sim[i-1], a_values)
+        theta_tilde = theta(d_given, a_tilde)
+
+        num = a_util(a_tilde, theta_tilde)
+        den = a_util(a_sim[i-1], theta_sim)
+
+        if np.random.uniform() <= num/den:
+            a_sim[i] = a_tilde
+            theta_sim = theta_tilde
+        else:
+            a_sim[i] = a_sim[i-1]
+
+    return(mode(a_sim[int(burnin*N_inner):])[0][0])
+
+
+def aps_atk_def(d_values, a_values, d_util, a_util, theta, N_aps=1000,
+                burnin=0.75, N_inner = 4000, verbose=False):
+
+    """ Computes the solution of an attacker-defender game using APS
+
+        Parameters
+        ----------
+        d_values : array-like
+            Strategies for the defender
+        a_values : array-like
+            Strategies for the attacker
+        d_util : callable
+            Computes the utility of the defender
+
+                ``d_util(d, theta) -> array, shape (same as theta)``
+
+            where theta is a 1-D array and d is the decisions of the defender
+        a_util : callable
+            Computes the utility of the attacker
+
+                ``a_util(a, theta) -> array, shape (same as theta)``
+
+            where theta is a 1-D array and a is the decision of the attacker
+        theta : callable
+            Draws samples of theta from the distribution p(theta | d, a)
+
+                ``theta(d, a, size=1) -> array, shape (size,)``
+
+            where d and a are the decisions for the defender and the attacker
+        N_aps : int, optional (default=1000)
+            Number of iterations for the APS algorithm
+        burnin : float, optional (default=0.75)
+            Percentage of burn-in iterations in the APS and APS inner algorithms
+    """
+
+    ################################################################################
+    ### Proposal of new d
+    def propose(d_given):
+        if d_given == d_values[0]:
+            return( np.random.choice([d_values[1], d_values[-1]],
+            p=[0.5, 0.5]) )
+
+        if d_given == d_values[-1]:
+            return( np.random.choice([d_values[0], d_values[-2]],
+            p=[0.5, 0.5]) )
+
+        idx = list(d_values).index(d_given)
+        return( np.random.choice([d_values[idx+1], d_values[idx-1]],p=[0.5, 0.5]) )
+
+    ## Compute a* for all d
+    if verbose:
+        print("PREPROCESSING...")
+    a_star = np.zeros_like(d_values)
+    for i, d in enumerate(d_values):
+        a_star[i] = innerAPS(d, a_values, a_util, theta, N_inner, burnin)
+
+
+    ## Start d_sim, vector of length N_aps to save in it simulation results
+    d_sim = np.zeros(N_aps, dtype = int)
+    d_sim[0] = np.random.choice(d_values)
+    a_sim = a_star[d_values == d_sim[0]][0]
+    theta_sim = theta(d_sim[0], a_sim)
+
+    if verbose:
+        print("START")
+
+    for i in range(1,N_aps):
+        ## Update d
+        d_tilde = propose(d_sim[i-1])
+        a_tilde = a_star[d_values == d_tilde][0]
+        theta_tilde = theta(d_tilde, a_tilde)
+
+        num = d_util(d_tilde, theta_tilde)
+
+        den = d_util(d_sim[i-1], theta_sim)
+
+
+        if np.random.uniform() <= num/den:
+            d_sim[i] = d_tilde
+            a_sim = a_tilde
+            theta_sim = theta_tilde
+        else:
+            d_sim[i] = d_sim[i-1]
+
+        if verbose:
+            if i%1000 == 0:
+                print(i)
+
+    return(mode(d_sim[int(burnin*N_aps):])[0], a_star)
+
+
+def aps_ara(d_values, a_values, d_util, a_util_f, theta_d, a_prob_f,
+            N_aps=1000, burnin=0.75, N_inner = 1000, J = 100, verbose=False):
+    """ Computes the solution of ARA using MCMC
+
+        Parameters
+        ----------
+        d_values : array-like
+            Strategies for the defender
+        a_values : array-like
+            Strategies for the attacker
+        d_util : callable
+            Computes the utility of the defender
+
+                ``d_util(d, theta) -> array, shape (same as theta)``
+
+            where theta is a 1-D array and d is the decision of the defender
+        a_util_f : callable
+            Returns a random function
+
+                ``a_util_f() -> callable``
+
+            with signature
+
+                ``a_util(a, theta) -> array, shape (same as theta)``
+
+            where theta is a 1-D array and a is the decision of the attacker
+        theta_d : callable
+            Draws samples of theta from the distribution p_d(theta | d, a)
+
+                ``theta_d(d, a, size=1) -> array, shape (size,)``
+
+            where d and a are the decisions for the defender and the attacker
+        a_prob_f : callable
+            Returns random function
+
+                ``a_prob_f() -> callable``
+
+            with signature
+
+                ``a_prob(d, a, size=1) -> array, shape (size,)``
+
+            where d and a are the decisions of the defender and the attacker
+        N_aps : int, optional (default=1000)
+            Number of iterations for the APS algorithm
+        burnin : float, optional (default=0.75)
+            Percentage of burn-in iterations in the APS and APS inner algorithms
+        J : int, optional (default=100)
+            Number of inner iterations to compute the random optimal
+            attacks distribution
+    """
+    ################################################################################
+    ### Proposal of new d
+    def propose(d_given):
+        if d_given == d_values[0]:
+            return( np.random.choice([d_values[1], d_values[-1]],
+            p=[0.5, 0.5]) )
+
+        if d_given == d_values[-1]:
+            return( np.random.choice([d_values[0], d_values[-2]],
+            p=[0.5, 0.5]) )
+
+        idx = list(d_values).index(d_given)
+        return( np.random.choice([d_values[idx + 1], d_values[idx - 1] ],p=[0.5, 0.5]) )
+
+    ## Compute a* for all d
+    if verbose:
+        print("PREPROCESSING...")
+    p_d = np.zeros((len(d_values), len(a_values)), dtype=float)
+
+    for i, d_given in enumerate(d_values):
+        modes = np.zeros(J, dtype=int)
+        for jj in range(J):
+            modes[jj] = innerAPS( d_given, a_values, a_util_f(),
+            a_prob_f(d_given), N_inner, burnin )
+        p_d[i, :] = np.bincount(modes, minlength = len(a_values))/J
+
+    d_sim = np.zeros(N_aps, dtype = int)
+    d_sim[0] = np.random.choice(d_values)
+    a_sim = np.random.choice(a_values, p=p_d[d_values == d_sim[0], :][0])
+    theta_sim = theta_d(d_sim[0], a_sim)
+
+    if verbose:
+        print("START")
+
+    for i in range(1,N_aps):
+        ## Update d
+        d_tilde = propose(d_sim[i-1])
+        a_tilde = np.random.choice(a_values, p=p_d[d_values == d_tilde, :][0])
+        theta_tilde = theta_d(d_tilde, a_tilde)
+
+        num = d_util(d_tilde, theta_tilde)
+
+        den = d_util(d_sim[i-1], theta_sim)
+
+
+        if np.random.uniform() <= num/den:
+            d_sim[i] = d_tilde
+            a_sim = a_tilde
+            theta_sim = theta_tilde
+        else:
+            d_sim[i] = d_sim[i-1]
+
+        if verbose:
+            if i%1000 == 0:
+                print(i)
+
+    return(mode(d_sim[int(burnin*N_aps):])[0], p_d)
---
+++ prob2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import math
+import numpy as np
+import pandas as pd
+from scipy.stats import uniform, gamma, beta, binom, norm
+
+CA_MAX = 1.5e6
+CA_MIN = -4.33e6
+
+unif = lambda a, b, size=1: uniform.rvs(a, b-a, size=size)
+
+a_values = np.arange(31)
+d_values = np.array([0, 2, 5, 10, 1000])
+
+d_cost = np.array([0, 2400, 3600, 4800, 12000])
+cs = lambda d: d_cost[d_values == d]
+
+def pl(a, d, a_g, scale_g, a_l, scale_l, size=1):
+    return (np.where(gamma.rvs(a=a_g, scale=scale_g, size=a*size) - d > 0,
+                     gamma.rvs(a=a_l, scale=scale_l, size=a*size), 0)
+              .reshape((size, a))
+              .sum(axis=1))
+
+def pm(l, alpha, beta):
+    return np.minimum(1.5e6, l*unif(alpha, beta))
+
+def ct(a, p):
+    t = binom.rvs(a, p=p) > 0
+    return norm.rvs(2430000, 400000) * t
+
+cd = lambda d, l, alpha, beta: cs(d) + pm(l, alpha=alpha, beta=beta)
+ud = lambda cd: (1/(math.e-1))*(np.exp(1 - cd/7e6) - 1)
+
+ca = lambda a, l, p, alpha, beta: pm(l, alpha=alpha, beta=beta)-ct(a, p=p)-792*a
+ua = lambda ca, ka=1: ((ca - CA_MIN)/(CA_MAX - CA_MIN)) ** ka
+
+prob   = lambda d, a, size=1: pl(a, d, 5, 1, 4, 1, size=size)
+d_util = lambda d, theta: ud(cd(d, theta, 0.0026, 0.00417))
+a_util = lambda a, theta: ua(ca(a, theta, 0.002, 0.0026, 0.00417), ka=9)
+
+def a_util_f():
+    p        = beta.rvs(2, 998)
+    a_param  = unif(0.0021, 0.0031)
+    b_param  = unif(0.00367, 0.00467)
+    ka       = unif(8, 10)
+    return lambda a, theta: ua(ca(a, theta, p, a_param, b_param), ka=ka)
+
+def a_prob_f(d):
+    a_g     = unif(4.8, 5.6)
+    a_l     = unif(3.6, 4.8)
+    scale_g = unif(0.8, 1.2)
+    scale_l = unif(0.8, 1.2)
+    return lambda d, a, size=1: pl(d, a, a_g, scale_g, a_l, scale_l, size=size)
