1d3d6fb8b5e8bb7e69a93bdade9682e3732d4617
albertotb <albertotb@gmail.com>
2018-05-10 18:36:22 +0200

    Fix attacker earning

--- prob2.py
+++ prob2.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 from scipy.stats import uniform, gamma, beta, binom, norm
 
-CA_MAX = 0
-CA_MIN = -4.5e6 -792*30
+CA_MAX = 1.5e6
+CA_MIN = -4.5e6-792*30+1.5e6
 
 CD_MIN = 0
 CD_MAX = 1.5e6 + 12000
@@ -34,7 +34,7 @@ def pl(a, d, a_g, scale_g, a_l, scale_l, size=1):
               .sum(axis=1))
 
 def pm(l, alpha, beta):
-    return np.minimum(1.5e6, l*unif(alpha, beta))
+    return np.minimum(1.5e6, 3e6*l*unif(alpha, beta))
 
 def ct(a, p):
     t = binom.rvs(a, p=p) > 0
@@ -44,7 +44,7 @@ cd = lambda d, l, alpha, beta: cs(d) + pm(l, alpha=alpha, beta=beta)
 ud = lambda cd: (1/(math.e-1))*(np.exp(1 - cd/CD_MAX) - 1)
 
 ca = lambda a, l, p, alpha, beta: pm(l, alpha=alpha, beta=beta)-ct(a, p=p)-792*a
-ua = lambda ca, ka=1: (ca - CA_MIN)/(CA_MAX - CA_MIN) ** ka
+ua = lambda ca, ka=1: ((ca - CA_MIN)/(CA_MAX - CA_MIN)) ** ka
 
 prob   = lambda d, a, size=1: pl(a, d, 5, 1, 4, 1, size=size)
 d_util = lambda d, theta: ud(cd(d, theta, 0.0026, 0.00417))
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 
     print(ca_min.min())
     print(ca_max.max())
-    
+
     #for n in range(1000):
     #    assert (np.array([ (a_util(a, l_values) > 0).all() for a in a_values ]) > 0).all()
     #    assert (np.array([ (d_util(d, l_values) > 0).all() for d in d_values ]) > 0).all()
