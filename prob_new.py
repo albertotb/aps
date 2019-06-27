54c75913241522f596d29a70c2ed9f4deec04972
Alberto Torres <albertotb@gmail.com>
2019-06-27 16:56:33 +0200

    normalize utility to [0, 1]

--- prob_new.py
+++ prob_new.py
@@ -28,25 +28,41 @@ beta  = lambda d,a: d - a + 1 + eps
 
 prob   = lambda d, a, size=1: np.random.beta( alpha(d,a), beta(d,a), size=size )
 
-n1 = 1 - np.exp(-h*D)
-n2 = 1 - np.exp(-h*c)
+c1 = 1 - np.exp(-h*D)
+c2 = 1 - np.exp(h*c)
+
+e1 = np.exp(-k*D)
+e2 = np.exp(k*e)
+
 ##
-h1 = -1 / (n2-n1)
-h2 =  n2 / (n2 - n1)
+h1 =   1 / (c1 - c2)
+h2 = -c2 / (c1 - c2)
+
+k1 = 1 / (e1 - e2)
+k2 = -e2 / (e1 - e2)
 
 d_util = lambda d, theta: h1*(1.0 - np.exp( -h * f(d,theta) ) ) + h2
-a_util = lambda a, theta: np.exp(-k * g(a,theta) )
+a_util = lambda a, theta: k1*(np.exp(-k * g(a,theta) )) + k2
 
 
 if __name__ == '__main__':
 
+    a_values = np.linspace(0,1, num=1000, endpoint=True)
     d_values = np.linspace(0,1, num=1000, endpoint=True)
     theta_values = np.linspace(0,1, num=1000, endpoint=True)
-    # check all utils are positive
-    res = np.zeros((len(d_values), len(theta_values)))
-    for i, d in enumerate(d_values):
-        for j, theta in enumerate(theta_values):
-            res[i, j] = d_util(d, theta)
 
-    print("Max Defender Utility", np.argmax(res))
-    print("Min Defender Utility", np.min(res))
+    # check all utils are positive
+    a_res = np.zeros((len(theta_values), len(a_values)))
+    d_res = np.zeros((len(theta_values), len(d_values)))
+    for i, theta in enumerate(theta_values):
+        for j, d in enumerate(d_values):
+            d_res[i, j] = d_util(d, theta)
+
+        for j, a in enumerate(a_values):
+            a_res[i, j] = a_util(a, theta)
+
+    print("Max Defender Utility", np.max(d_res))
+    print("Min Defender Utility", np.min(d_res))
+
+    print("Max Attacker Utility", np.max(a_res))
+    print("Min Attacker Utility", np.min(a_res))
