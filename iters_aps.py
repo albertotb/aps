e67d4071079cca6243bf5a54be1af8f074c1a6e0
roinaveiro <roi.naveiro@gmail.com>
2019-09-12 00:28:32 +0200

    time exps

--- iters_aps.py
+++ iters_aps.py
@@ -14,21 +14,19 @@ import math
 
 def optimal_number_iters(d_values, a_values, d_true, disc, times=10, n_jobs=-1):
 
-    temp_inner_list = [10, 40, 100, 1000, 10000]       # 40    1000
-    temp_outer_list = [10, 40, 1000, 100000, 200000]   # 40  100000
-    iter_inner_list = [10, 50, 100, 1000]              # 50     100
-    iter_outer_list = [10, 50, 100, 1000, 10000]       # 50    1000
+    temp_inner_list = [100]       # 40    1000
+    temp_outer_list = np.arange(1000, 10000, 100)#[10, 40, 1000, 100000, 200000]   # 40  100000
+    iter_inner_list = [10, 50, 100]              # 50     100
+    iter_outer_list = [300, 400, 500]       # 50    1000
 
     # the lists have to be sorted in the product from less impact to more
     # impact in the complexity of the algorithm
-    for temp_outer, temp_inner, iter_outer, iter_inner in product(temp_inner_list,
-                                                                  temp_outer_list,
-                                                                  iter_inner_list,
-                                                                  iter_outer_list):
+    prd = product(iter_outer_list, temp_outer_list, iter_inner_list, temp_inner_list)
+    for iter_outer, temp_outer, iter_inner, temp_inner in prd:
         def find_d_opt():
             d_opt = aps_adg_ann(temp_outer, temp_inner, p.d_util, p.a_util,
                                 p.prob, N_aps=iter_outer, N_inner=iter_inner,
-                                burnin=0.5, prec=disc, mean=True, info=False)
+                                burnin=0.05, prec=disc, mean=True, info=False)
             return d_opt
 
         optimal_d = Parallel(n_jobs=n_jobs)(
@@ -38,6 +36,8 @@ def optimal_number_iters(d_values, a_values, d_true, disc, times=10, n_jobs=-1):
         optimal_d = np.round(optimal_d, int(-np.log10(disc)))
         percent = np.mean(np.isclose(np.array(optimal_d), d_true, rtol=disc))
 
+        print("iter_outer: " + str(iter_outer) + "temp_outer: "+ str(temp_outer) + "iter_inner: " + str(iter_inner) + "temp_inner: " + str(temp_inner))
+        print("percent:", percent)
         # percent = np.mean(
         #    np.isclose(np.array(optimal_d), d_true, rtol=disc)
         #)
@@ -50,22 +50,22 @@ def optimal_number_iters(d_values, a_values, d_true, disc, times=10, n_jobs=-1):
 
 if __name__ == '__main__':
 
-    disc_list = np.array([0.1, 0.01])
+    disc_list = np.array([0.01])
 
     results = []
     for disc in disc_list:
         a_values = np.arange(0, 1, disc)
         d_values = np.arange(0, 1, disc)
 
-        d_true = mcmc_adg(d_values, a_values, p.d_util, p.a_util, p.prob,
-                          p.prob, mcmc_iters=1000000, info=False)
-
+        #d_true = mcmc_adg(d_values, a_values, p.d_util, p.a_util, p.prob,
+        #                  p.prob, mcmc_iters=1000000, info=False)
+        d_true = 0.46
         temp_outer, temp_inner, iter_outer, iter_inner = optimal_number_iters(
                 d_values, a_values, d_true, disc, n_jobs=10)
 
         start = default_timer()
         d_opt = aps_adg_ann(temp_outer, temp_inner, p.d_util, p.a_util, p.prob,
-                            N_aps=iter_outer, N_inner=iter_inner, burnin=0.5,
+                            N_aps=iter_outer, N_inner=iter_inner, burnin=0.2,
                             prec=disc, mean=True, info=False)
         end = default_timer()
         time = end-start
@@ -79,4 +79,4 @@ if __name__ == '__main__':
               d_true, d_opt)
 
     df = pd.DataFrame(results)
-    df.to_csv('results/iters_aps.csv', index=False)
+    df.to_csv('results/iters_aps_roi_2.csv', index=False)
