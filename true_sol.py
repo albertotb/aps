509506ff7f872bf440f2c2fd9f1d2c40b39542c2
Alberto Torres <albertotb@gmail.com>
2019-09-17 21:51:28 +0200

    add script to compute true solution with MCMC

---
+++ true_sol.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import prob_new as p
+from mcmc import *
+from joblib import Parallel, delayed
+
+disc   = 0.001
+times  = 50
+iters  = 1000000
+inner  = 1000
+n_jobs = 16
+
+a_values = np.arange(0, 1, disc)
+d_values = np.arange(0, 1, disc)
+
+def find_d_opt():
+    d_opt = mcmc_adg(d_values, a_values, p.d_util, p.a_util, p.prob,
+                     p.prob, iters=iters, inner_iters=inner, info=False)
+    return d_opt
+
+d_opt = Parallel(n_jobs=n_jobs)(
+           delayed(find_d_opt)() for j in range(times)
+)
+
+after_dot = str(disc).split('.', 1)[-1]
+np.save(f'sol_{after_dot}.npy', np.array(d_opt))
