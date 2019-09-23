38aa62ed72f0d4de6d651edc9f46c8d9b1d0b2a8
albertotb <albertotb@gmail.com>
2019-09-23 18:24:53 +0200

    organization

--- plots.R
+++ plots.R
@@ -19,10 +19,10 @@ dist = read.csv("results/dist_APS.csv")
 p = ggplot(dist, aes(x = X0)) + geom_histogram(aes(y=(..count..)/sum(..count..)), bins = 40, colour="black", fill="white")
 p = p + geom_vline(aes(xintercept=125),
                    color="blue", linetype="dashed", size=1)
-p = p +  theme_bw() + xlab("Defender's Decision") + ylab("Frequency")
+p = p + xlab("Defender's Decision") + ylab("Frequency")
 p
 
-ggsave(p, filename = "img/aps_prob3.eps", device = "eps", dpi = 300)
+ggsave(p, filename = "img/aps_prob3.pdf", device = "pdf", dpi = 300)
 
 ## Expected utility time comparison problem
 
@@ -30,10 +30,10 @@ ggsave(p, filename = "img/aps_prob3.eps", device = "eps", dpi = 300)
 
 psi_d = read.csv("results/EU_timecompprob.csv")
 p = ggplot(psi_d, aes(x = d, y = EU)) + geom_bar(stat="identity", colour="black", fill = "white") 
-p = p +  theme_bw() + xlab("Defender's Decision") + ylab("Defender's Expected Utility")
+p = p + xlab("Defender's Decision") + ylab("Defender's Expected Utility")
 p
 
-ggsave(p, filename = "img/EU_probtime.eps", device = "eps", dpi = 300)
+ggsave(p, filename = "img/EU_probtime.pdf", device = "pdf", dpi = 300)
 
 
 ## Cost interpolation
@@ -43,10 +43,10 @@ ggsave(p, filename = "img/EU_probtime.eps", device = "eps", dpi = 300)
 costs = read.csv("costs.csv")
 costs = costs[costs$x <= 200,]
 p = ggplot(costs, aes(x = x, y = y)) + geom_line()
-p = p +  theme_bw() + xlab("Amount of protection in gbps") + ylab("Cost in Euros")
+p = p + xlab("Amount of protection in gbps") + ylab("Cost in Euros")
 p
 
-ggsave(p, filename = "img/costs.eps", device = "eps", dpi = 300)
+ggsave(p, filename = "img/costs.pdf", device = "pdf", dpi = 300)
 
 ## p_d real problem
 
@@ -57,5 +57,5 @@ ds = c(0, 50, 100, 195)
 pd_melted = pd_melted[pd_melted$d %in% ds,]
 p = ggplot(data = pd_melted, aes(x = variable, y = value)) + geom_bar(stat = "identity", colour="black", fill = "white")
 p = p + facet_wrap(~ d, nrow = 2)
-p = p + xlab("Attacker's Decision") + ylab("p(a|d)") + theme_bw()
-ggsave(p, filename = "img/pa_given_d.eps", device = "eps", dpi = 300)
+p = p + xlab("Attacker's Decision") + ylab("p(a|d)")
+ggsave(p, filename = "img/pa_given_d.pdf", device = "pdf", dpi = 300)
